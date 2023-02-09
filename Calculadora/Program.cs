024e3320ac3b65468f3a0421746b83f0357a16fd
mariza66 <120022387+mariza66@users.noreply.github.com>
2023-02-09 11:54:45 -0300

    Add files via upload

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,8 +13,8 @@ namespace Calculadora
         {
             double valor1 = 0.0;
             double valor2 = 0.0;
-            int opcao = 0;
-            
+
+
             Console.WriteLine("digite o primeiro valor");
             valor1 = Convert.ToDouble(Console.ReadLine());
 
@@ -21,42 +22,96 @@ namespace Calculadora
             valor2 = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Escolha uma operação:");
-            Console.WriteLine("1- Soma");
-            Console.WriteLine("2- Subtração");
-            Console.WriteLine("3- Divisão");
-            Console.WriteLine("4- Multiplicação");
+            Console.WriteLine("    1- Soma");
+            Console.WriteLine("    2- Subtração");
+            Console.WriteLine("    3- Divisão");
+            Console.WriteLine("    4- Multiplicação");
             Console.WriteLine("5- Nada,só estou dando uma olhadinha" + "\n");
-
-            if (opcao == 1)
+            switch (Console.ReadLine())
             {
-                Console.WriteLine("digite essa opção");
-                opcao = Convert.ToInt16(Console.ReadLine());
 
-                if (opcao == 2)
+                case "1":
+
+                    Console.WriteLine(valor1 + valor2);
+                    break;
+
+
+
+
+
+
+
+                case "2":
+
+
                     Console.WriteLine(valor1 - valor2);
+                    break;
 
-                if (opcao == 3) ;
-                Console.WriteLine(valor1 / valor2);
 
-                if (opcao == 4)
+
+
+
+
+
+                case "3":
+
+
+                    if (valor1 >= valor2)
+
+                    {
+
+                        Console.WriteLine("resultado é " + (valor1 / valor2));
+
+                    }
+                    else
+
+                    {
+                        Console.WriteLine("invalido");
+
+                    }
+
+                    break;
+                case "4":
+
                     Console.WriteLine(valor1 * valor2);
+                    break;
 
-                if (opcao == 5) ;
 
-                Console.WriteLine("EXIT");
-                opcao = (int)Convert.ToDouble(Console.ReadLine);
+
+
+
+                case "5":
+                    break;
+
+
+
+            }
+
+        }
+
+
+    }
+}
+       
+     
+
+
+
+
+
+
+
+
 
                    
 
 
-            }   
-                
-
-               
-
-             
+                    
+                   
+                   
 
 
-        }
-    }
-}
+
+
+
+          
